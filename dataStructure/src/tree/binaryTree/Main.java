be2e9c817a7ad5e987e28360692807199e77801d
osama khan <osk@7462@gmail.com>
2021-01-11 00:02:24 +0545

    following methods has been added 1.search(),  2.delete(),  3.printLevelOrder(),  4.depth()

--- dataStructure/src/tree/binaryTree/Main.java
+++ dataStructure/src/tree/binaryTree/Main.java
@@ -3,18 +3,33 @@ package tree.binaryTree;
 public class Main {
     public static void main(String[] args) {
         int[] data = new int[]{10,20,30,40,50,60,70,80,90,100};
+
+//      creating a tree
         Tree tree = new Tree();
+
+
         tree.insertLevelOrder(data);
 
-        System.out.println("preOrder");
+        tree.depth();
+
+        tree.search(1000);
+
+        tree.delete(100);
+        tree.delete(1000);
+
+        System.out.print("printLevelOrderTraversal():\t");
+        tree.printLevelOrderTraversal();
+        System.out.println();
+
+        System.out.print("printPreOrder():\t");
         tree.printPreOrder();
         System.out.println();
 
-        System.out.println("inOrder");
+        System.out.print("printInOrder():\t");
         tree.printInOrder();
         System.out.println();
 
-        System.out.println("PostOrder");
+        System.out.print("printPostOrder():\t");
         tree.printPostOrder();
         System.out.println();
 
--- dataStructure/src/tree/binaryTree/Tree.java
+++ dataStructure/src/tree/binaryTree/Tree.java
@@ -1,6 +1,9 @@
 package tree.binaryTree;
 
 import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Tree {
     Node root;
@@ -19,7 +22,7 @@ public class Tree {
         this.root = insertLevelOrder(this.root, data, 0, data.length);
     }
     public Node insertLevelOrder(Node root, int[] data, int i, int n) {
-        
+
         if (i < n) {
             root = new Node(data[i]);
             root.setLeft(insertLevelOrder(root.getLeft(), data, i*2+1, n));
@@ -27,6 +30,81 @@ public class Tree {
         }
         return root;
     }
+
+    public void search(int data) {
+        if (printLevelOrderTraversal(this.root,data,true))
+            System.out.println("search()\t"+ data +" found");
+        else
+            System.out.println("search():\t"+ data +" not found");
+    }
+
+    public void delete(int data) {
+        Node deleteNode = null;
+        Node leafNode = null;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(this.root);
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (node.getData() == data)
+                deleteNode = node;
+            if (node.getLeft() != null) {
+                queue.add(node.getLeft());
+            }
+            if (node.getRight() != null) {
+                queue.add(node.getRight());
+            }
+
+            if (node.getLeft() == null && node.getRight() == null) {
+                leafNode = node;
+            }
+        }
+        if (deleteNode != null) {
+            deleteNode.setData(leafNode.getData());
+            System.out.println("delete():\t" + leafNode.getData() + " successfully deleted ");
+            leafNode = null;
+        } else {
+            System.out.println("delete():\t" + data +" is not found");
+        }
+    }
+
+    public void depth() {
+        System.out.println("depth():\tthe depth/height of this tree is: " + depth(this.root));
+    }
+    public int depth(Node root) {
+        if (root == null)
+            return 0;
+
+        int lDepth = depth(root.getLeft());
+        int rDepth = depth(root.getRight());
+        if (lDepth > rDepth)
+            return lDepth+1;
+        else return rDepth +1;
+    }
+
+    public void printLevelOrderTraversal() {
+        printLevelOrderTraversal(this.root,0,false);
+    }
+    public boolean printLevelOrderTraversal(Node root, int searchData, boolean search) {
+        boolean isFound = false;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (!search)
+                System.out.print(node.getData() + ",");
+//            this is for search purpose we can also add another method for search.
+            if (node.getData() == searchData)
+                isFound = true;
+            if (node.getLeft() != null) {
+                queue.add(node.getLeft());
+            }
+            if (node.getRight() != null) {
+                queue.add(node.getRight());
+            }
+        }
+        return isFound;
+    }
+
 
     public void printPreOrder() {
         printPreOrder(this.root);
